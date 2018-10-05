e0bbaca4dc2c357850694aeb1cafa6eaf65fb193
GreggC95 <43067498+GreggC95@users.noreply.github.com>
2018-10-05 14:26:48 +0100

    Added walls ground and items

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    //Public variables visible in Unity
+    public TextMesh displayText;
+
+    //private variables can't be touched by other scripts
+    private int currentValue = 0;
+    
+    //update both the data value And the visual display
+    public void ChangeValue(int _toChange)
+    {
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+    }
+
+    //reset the score to zero
+    public void ResetScore()
+    {
+        currentValue = 0;
+        displayText.text = currentValue.ToString();
+    }
+}
---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour {
+
+    public Score scoreTracker;
+    public int scoreValue = 1;
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //ony do 
+        if (collision.GetComponent<Player>())
+        {
+            //updating the score based on how nuch this item is worth
+            scoreTracker.ChangeValue(scoreValue);
+
+
+            //get rid of the item (it's been used up) 
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
